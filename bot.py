39e29776647eeba5ad886351c5b52b67c8b9ae37
Ibragimov Diyorbek <59175919+diyorbekibragimov@users.noreply.github.com>
2021-11-01 09:55:46 +0500

    setting up a webhook

--- bot.py
+++ bot.py
@@ -1,9 +1,14 @@
 from aiogram import executor
 from dispatcher import dp
 import handlers
+import os, config
 
 from db import BotDB
 BotDB = BotDB('fincontrol.db')
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=False)
+    PORT = int(os.environ.get("PORT", 5000))
+    executor.start_webhook(listen="0.0.0.0", 
+                            port=int(PORT),
+                            url_path=config.BOT_TOKEN)
+    executor.bot.setWebhook("https://control-finance-bot.herokuapp.com/" + config.BOT_TOKEN)
