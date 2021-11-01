bfbe3388b7f266021394bc5efa76476987b5a2bc
Diyorbek <dierbekibragimov@gmail.com>
2021-11-01 13:23:37 +0500

    fixed bugs

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os
 from dotenv import load_dotenv
-from aiogram.types import ContentTypes
 
 load_dotenv()
 
@@ -8,10 +7,10 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 URL = os.getenv('EXCHANGE_URL')
 HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
-WEBHOOK_HOST = f'{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
 WEBHOOK_PORT = int(os.getenv("WEBHOOK_PORT"))
-WEBHOOK_URL_PATH = "/webhook"
-WEBHOOK_URL = f'https://{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_URL_PATH}'
+WEBHOOK_PATH = os.getenv("WEBHOOK_PATH")
+WEBHOOK_URL = f'{WEBHOOK_HOST}:{WEBHOOK_PORT}{WEBHOOK_PATH}'
 
 WEBAPP_HOST = '0.0.0.0'
 WEBAPP_PORT = int(os.getenv('WEBAPP_PORT'))
