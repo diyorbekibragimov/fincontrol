9fa7f6a91e5275f7282d74d52626e92fafcc6d20
Diyorbek <dierbekibragimov@gmail.com>
2021-11-04 09:58:47 +0500

    fixed bugs

--- db.py
+++ db.py
@@ -6,10 +6,10 @@ class BotDB:
 
     def __init__(self):
         self.conn = psycopg2.connect(
-                host={DB_HOST},
-                database={DB_NAME},
-                user={DB_USER},
-                password={DB_PASSWORD}
+                host=DB_HOST,
+                database=DB_NAME,
+                user=DB_USER,
+                password=DB_PASSWORD
             )
         self.cursor = self.conn.cursor()
         self.converter = RealTimeCurrencyConverter(URL)
